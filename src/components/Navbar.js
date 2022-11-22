ee269dcfafe23f25a8fdb8228eb0db30fded7766
muyed moktadir <muyed004@gmail.com>
2022-11-22 09:22:03 +0600

    add book now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,8 @@ function Navbar({ children }) {
             <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
             <div class="drawer-content flex flex-col">
                 {/* <!-- Navbar --> */}
-                <div class="w-full navbar bg-base-300">
-                    <div class="flex-1 px-2 mx-2">Heal-Scape</div>
+                <div class="w-full navbar bg-base-100 px-20">
+                    <div class="flex-1 px-2 mx-2 text-2xl">Heal-Scape</div>
                     <div class="flex-none lg:hidden">
                         <label for="my-drawer-3" class="btn btn-square btn-ghost">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
@@ -23,6 +23,14 @@ function Navbar({ children }) {
                             <li><NavLink to='/about' className='rounded-lg'>About</NavLink></li>
                             <li><NavLink to='/contact' className='rounded-lg'>Contact Us</NavLink></li>
                             <li><NavLink to='/login' className='rounded-lg'>Login</NavLink></li>
+                            <li class="dropdown dropdown-end dropdown-hover">
+                                <label tabindex="0" class="btn btn-outline rounded-lg hover:bg-red-500">Book Now</label>
+                                <ul tabindex="0" class="dropdown-content menu p-2 my-1 shadow bg-base-100 rounded-box w-52">
+                                    <li><a>Item 1</a></li>
+                                    <li><a>Item 2</a></li>
+                                </ul>
+
+                            </li>
                         </ul>
                     </div>
                 </div>
